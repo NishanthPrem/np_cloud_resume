c650a496899431be7da1728a6c247709f9d78aba
Nishanth Prem <nishanthprem8@gmail.com>
2024-10-28 11:45:46 -0700

    Imported S3 and dynamodb to terraform.

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-west-1"
 }
+
+resource "aws_s3_bucket" "resume_bucket" {
+  bucket = "np-cloud-resume"
+}
+
+resource "aws_dynamodb_table" "resume_table" {
+  name = "np-cloud-resume-db"
+}
