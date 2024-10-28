c8c2e5d804b24a4ce663c1cf1ca1413dad81139f
Nishanth Prem <nishanthprem8@gmail.com>
2024-10-28 13:53:36 -0700

    Imported Cloudfront to terraform.

--- main.tf
+++ main.tf
@@ -35,8 +35,53 @@ resource "aws_lambda_function" "resume_lambda" {
   memory_size   = 128
   timeout       = 3
   filename      = "function.zip"
-  
+
   lifecycle {
     ignore_changes = [filename]
+  }
+}
+
+resource "aws_cloudfront_distribution" "resume_cloudfront" {
+  enabled       = true
+  price_class   = "PriceClass_All"
+  http_version  = "http2and3"
+  is_ipv6_enabled = true
+  retain_on_delete = false
+
+  origin {
+    domain_name = "np-cloud-resume.s3-website-us-west-1.amazonaws.com"
+    origin_id   = "np-cloud-resume.s3-website-us-west-1.amazonaws.com"
+
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_protocol_policy   = "http-only"
+      origin_ssl_protocols     = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_read_timeout      = 30
+      origin_keepalive_timeout = 5
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "np-cloud-resume.s3-website-us-west-1.amazonaws.com"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    default_ttl            = 0
+    min_ttl                = 0
+    max_ttl                = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1"
   }
 }
